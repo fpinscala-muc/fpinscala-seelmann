2b47612dd7171226ccad38699183ee397843c363
Stefan Seelmann <mail@stefan-seelmann.de>
2014-10-29 08:25:50 +0100

    exercises chapter 6.4

--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -84,11 +84,21 @@ object RNG {
       (f(a, b), rng3)
     }
 
-  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = ???
+  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] =
+    // copied from answers
+    fs.foldRight(unit(List[A]()))((f, acc) => map2(f, acc)(_ :: _))
 
-  def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] = ???
+  def flatMap[A, B](f: Rand[A])(g: A => Rand[B]): Rand[B] =
+    rng => {
+      val (a, rng2) = f(rng)
+      g(a)(rng2)
+    }
 
-  def nonNegativeLessThan(n: Int): Rand[Int] = ???
+  def nonNegativeLessThan(n: Int): Rand[Int] =
+    flatMap(nonNegativeInt) { i =>
+      val mod = i % n
+      if (i + (n - 1) - mod >= 0) unit(mod) else nonNegativeLessThan(n)
+    }
 
   def mapViaFlatMap[A,B](s: Rand[A])(f: A => B): Rand[B] = ???
 
